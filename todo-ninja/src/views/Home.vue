669f70d0e356d2319a03292fa5281f27049210b2
unknown <webberic92@yahoo.com>
2021-08-16 11:50:39 -0400

    starting vuetify stuff.

---
+++ todo-ninja/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="home">
+<h1>Homepage</h1>
+<p class="red white--text">testest</p>
+<p class="pink lighten-4 red--text text--darken-4">testest</p>
+<h1>Massive Display</h1>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
