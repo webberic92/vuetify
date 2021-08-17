89ec8a1e1ac2394b408e57022d796666f809704b
unknown <webberic92@yahoo.com>
2021-08-17 16:25:28 -0400

    26 going on 27th lesson.

---
+++ todo-ninja/src/components/Popup.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">Add New Project</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field v-model="title" label="Title" prepend-icon="folder"></v-text-field>
+          <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+
+          <v-menu v-model="menu" :close-on-content-click="false">
+            <v-text-field slot="activator" 
+              :value="formattedDate" clearable label="Due date" prepend-icon="date_range">
+            </v-text-field>
+            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+
+          <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import format from 'date-fns/format'
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null,
+      menu: false
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate () {
+      console.log(this.due)
+      return this.due ? format(this.due, 'Do MMM YYYY') : ''
+    }
+  }
+}
+</script>
--- todo-ninja/src/views/Teams.vue
+++ todo-ninja/src/views/Teams.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="about">
-    <h1 class="subheading grey--text">This is the Teams page</h1>
-     <v-container class="my-5">
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae odit aut magnam! Laboriosam quia harum inventore veniam architecto. Rerum earum blanditiis perspiciatis ratione cupiditate atque impedit tempora fugit ab sed?</p>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae odit aut magnam! Laboriosam quia harum inventore veniam architecto. Rerum earum blanditiis perspiciatis ratione cupiditate atque impedit tempora fugit ab sed?</p>
+  <div class="team">
+    <h1 class="subheading grey--text">Team</h1>
 
- </v-container>
- 
+    <v-container class="my-5">
+
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span class="">Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+    </v-container>
+    
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        { name: 'Ducky Webb', role: 'Master', avatar: '/golduck.jpg' },
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+        { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
+      ]
+    }
+  }
+}
+</script>
